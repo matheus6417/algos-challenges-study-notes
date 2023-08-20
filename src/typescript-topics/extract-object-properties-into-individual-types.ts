017e811034e25083ddf834f6be537d7651271af0
Matheus Cezar <matheus6417@gmail.com>
2023-08-20 13:50:40 +0200

    Extract Object Properties into Individual Types

---
+++ src/typescript-topics/extract-object-properties-into-individual-types.ts
@@ -0,0 +1,50 @@
+// import { Equal, Expect } from "../helpers/type-utils";
+
+// export const fakeDataDefaults = {
+//   String: "Default string",
+//   Int: 1,
+//   Float: 1.14,
+//   Boolean: true,
+//   ID: "id",
+// };
+
+// export type StringType = unknown;
+// export type IntType = unknown;
+// export type FloatType = unknown;
+// export type BooleanType = unknown;
+// export type IDType = unknown;
+
+// type tests = [
+//   Expect<Equal<StringType, string>>,
+//   Expect<Equal<IntType, number>>,
+//   Expect<Equal<FloatType, number>>,
+//   Expect<Equal<BooleanType, boolean>>,
+//   Expect<Equal<IDType, string>>,
+// ];
+
+// Challenge
+// Your challenge is to turn fakeDataDefaults into a type, then extract each of the object into its individual pieces:
+
+import { Equal, Expect } from '../helpers/type-utils';
+
+export const fakeDataDefaults = {
+  String: 'Default string',
+  Int: 1,
+  Float: 1.14,
+  Boolean: true,
+  ID: 'id',
+};
+
+export type StringType = typeof fakeDataDefaults['String'];
+export type IntType = typeof fakeDataDefaults['Int'];
+export type FloatType = typeof fakeDataDefaults['Float'];
+export type BooleanType = typeof fakeDataDefaults['Boolean'];
+export type IDType = typeof fakeDataDefaults['ID'];
+
+type tests = [
+  Expect<Equal<StringType, string>>,
+  Expect<Equal<IntType, number>>,
+  Expect<Equal<FloatType, number>>,
+  Expect<Equal<BooleanType, boolean>>,
+  Expect<Equal<IDType, string>>
+];
